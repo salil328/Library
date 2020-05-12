9c5767f67e3938fa9f1663a7654490a8652924be
salil328 <57859993+salil328@users.noreply.github.com>
2020-05-12 18:25:32 +0530

    Add files via upload

---
+++ Library.js
@@ -0,0 +1,15 @@
+const formsubmit = ()=>{
+ var booktitle = document.getElementById("title").value
+    console.log(booktitle)  
+   }
+formsubmit()
+
+// const readdata = ()=>{
+//     var formdata = {
+//      "Title": document.getElementById("Title").value, 
+//       "Gener": document.getElementById("Gener").value,
+//        "Quantity" : document.getElementById("Quantity").value
+      
+//     }
+//     console.log(formdata)
+// }
